20700bef7999bd0e9f207e9066cdb81512539c97
Yousri <laifayousri@gmail.com>
2019-06-15 20:52:15 +0200

    Création du fichier script_1.js

---
+++ script_1.js
@@ -0,0 +1,17 @@
+/*
+Créé un script script_1.js qui affichera "Bonjour, monde !" dans la console. 
+Voici les lignes que tu dois voir dans ta console :
+"Bonjour monde"
+*/
+
+console.log("Bonjour monde!")
+
+/*
+Customise le script script_1.js qui te demandera ton prénom avec un prompt. 
+Tu stockeras cette entrée dans une variable et tu afficheras : 
+"Bonjour, prénom !" dans la console.
+*/
+
+let answer = "";
+console.log(answer = prompt("Quel est ton prénom?"))
+console.log("Bonjour" + " " + answer + "!")
